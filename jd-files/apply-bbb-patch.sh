4ec82ed3c14c16065664df25d65c3b7ccd55a1d4
Joe Desbonnet <jdesbonnet@gmail.com>
2016-03-23 21:50:11 +0000

    patch from github.com/eewiki/u-boot-patches/blob/master/v2014.04

---
+++ jd-files/apply-bbb-patch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Patch from:
+# https://github.com/eewiki/u-boot-patches/blob/master/v2014.04/0001-am335x_evm-uEnv.txt-bootz-n-fixes.patch
+#
+patch -p1 < 0001-am335x_evm-uEnv.txt-bootz-n-fixes.patch.txt
